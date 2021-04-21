5a258c7f2a080321b7fb17d85de66dfafe5175d4
GiuliaRosignoli <giuliarosignoli@gmail.com>
2021-04-21 19:00:44 +0200

    -

--- script/main.js
+++ script/main.js
@@ -12,13 +12,22 @@ const app = new Vue ({
             "https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg",
         ],
         indexPhoto: 0, // Current image position
+        intervalId: 0,
 
+    },
+    created(){
+        console.log("created");
+        this.startLoop();
     },
 
     methods: {
         prevPhoto(){
+            this.indexPhoto -= 1;
             
-        },
+            if(this.indexPhoto < 0) {
+                this.indexPhoto = this.photos.length - 1
+        }
+    },
         nextPhoto() {
             //console.log("Click next")
             this.indexPhoto += 1;
@@ -26,6 +35,18 @@ const app = new Vue ({
             if(this.indexPhoto > (this.photos.length - 1) ) {
                 this.indexPhoto = 0;
             }
+        },
+        setPhoto(index) {
+            console.log(index);
+            this.indexPhoto = index;
+        },
+        startLoop(){
+            this.intervalId = setInterval(() =>{
+                this.nextPhoto();
+            }, 3000);
+        },
+        stopLoop(){
+            clearInterval(this.intervalId);
         }
     } 
 });
